8fe6063e8629d0470db2ee2a185cc03fe040ce9f
jsr2k1 <jsr2k1@hotmail.com>
2015-04-06 17:54:41 +0200

    Cambios en el diseño de la interfaz.

---
+++ Assets/Editor/CustomImportSettings.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using UnityEditor.Sprites;
+
+public class CustomImportSettings : AssetPostprocessor 
+{
+	//TEXTURES
+	void OnPreprocessTexture()
+	{
+		TextureImporter textureImporter = assetImporter as TextureImporter;
+
+		//if(textureImporter.assetPath.Contains("Interface"))
+		{
+			textureImporter.textureType = TextureImporterType.Sprite;
+			textureImporter.textureFormat = TextureImporterFormat.AutomaticTruecolor;
+			textureImporter.mipmapEnabled = false;
+			textureImporter.maxTextureSize = 128;
+			textureImporter.filterMode = FilterMode.Point;
+		}
+	}
+	/*
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//AUDIOS
+	void OnPreprocessAudio()
+	{
+		AudioImporter audioImporter = assetImporter as AudioImporter;
+		
+		audioImporter.threeD = false;
+	}
+	*/
+	/*	
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//MODELS
+	void OnPreprocessModel()
+	{
+		ModelImporter modelImporter = assetImporter as ModelImporter;
+		modelImporter.generateSecondaryUV=true;
+	}
+	*/
+}

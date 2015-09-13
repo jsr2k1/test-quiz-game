192c68ea85c12dd8911febbe90eeee81418d748b
jsr2k1 <jsr2k1@hotmail.com>
2015-09-13 18:12:48 +0200

    more

--- Assets/Scripts/Translate.cs
+++ Assets/Scripts/Translate.cs
@@ -7,6 +7,6 @@ public class Translate : MonoBehaviour
 	void Start()
 	{
 		string id = gameObject.GetComponent<Text>().text;
-		gameObject.GetComponent<Text>().text = LanguageManager.GetText(id);
+		gameObject.GetComponent<Text>().text = LanguageManager.instance.GetText(id);
 	}
 }
